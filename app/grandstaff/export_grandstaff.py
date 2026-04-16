7b39489f4d24385d1cab150ed0352cb5808a9697
Jirka Mayer <jirkamayer97@gmail.com>
2026-04-16 10:35:56 +0200

    Elaborated on GrandStaff splits.json

--- app/grandstaff/export_grandstaff.py
+++ app/grandstaff/export_grandstaff.py
@@ -24,7 +24,7 @@ def export_grandstaff(
     #           /original_m-56-60.jpg
     #
     # Which corresponds to this scheme:
-    # {composer}/{genre}/{song}/original_m-{from}-{to}.jpg
+    # {composer}/{genre}/{song}/{transposition}_m-{from}-{to}.jpg
     #
     # Where "original" means "no transposition was done",
     # and {from} and {to} are measure indexes within the song.
@@ -36,29 +36,53 @@ def export_grandstaff(
     # in the following way:
     #
     # Page:
-    # {composer}_{song}/
+    # {composer}_{song}_{transposition}_m-{from}-{to}/
     #   Grandstaves/
-    #       original_m-56-60/
+    #       1-2/                        (only one grandstaff per "page")
     #           image.jpg
-    #           image_distorted.jpg
+    #           image.distorted.jpg
     #           transcription.krn
     #           transcription.musicxml
     #
-    # Basically, each song becomes a MusiCorpus page and each
-    # sample becomes one grandstaff subdivision of that page.
+    # Basically, each sample becomes a MusiCorpus page with
+    # exactly one grandstaff subdivision of that page being that sample.
     # We explicitly omit the {genre} as it has minimal
     # informational value and only lengthens page names unnecessarily.
     #
     # We include all available transpositions, not just the "original".
     #
-    # We include both .jpg and _distorted.jpg files and the .krn files.
+    # We include both .jpg and .distorted.jpg files and the .krn files.
     #
     # We include the .musicxml file from the Grandstaff LMX dataset at:
     # https://github.com/ufal/olimpic-icdar24/releases/tag/datasets
     # (must be added to CLI options)
     #
-    # We do not provide Staff subdivisions, because it is unclear
+    # We do NOT provide Staff subdivisions, because it is unclear
     # as to where to get the images.
+    #
+    # The splits.json file should be generated from
+    # samples.train/dev/test.txt files in the GrandStaff-LMX dataset.
+    # These were requested from the original GrandStaff paper authors
+    # and should correspond to their partitioning of the dataset.
+    # The official grandstaff.tgz file unfortunately does not
+    # contain these partitions.
+    #
+    # We decided to have one musicorpus page as one grandstaff sample,
+    # so that splits.json file could be provided. It would be semantically
+    # nicer to have one grandstaff song to become one musicorpus page,
+    # however that would require the slits.json to be song-consistent,
+    # which it isn't. We also discovered that there's an overlap not
+    # only in songs between train/test but also in measure ranges.
+    # However they always come from a different transposition.
+    # The test split contains ONLY "original" transpositions,
+    # while the augmented data is present in train and dev.
+    #
+    # The resulting PRAIG.GrandStaff folder will therefore contain
+    # cca 40K folders (pages = samples). While this is a lot and
+    # will cause slowdowns in file explorers, it is doable.
+    # The PrIMuS dataset developed by the same group as GrandStaff
+    # is disctributed as a .tgz archive with over 86K folders in
+    # the top-level folder.
 
     print("To be implemented...")
 
